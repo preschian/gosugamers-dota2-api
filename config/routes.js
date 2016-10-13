e397f706843a75acc53a80bd2db209f5d5e7706f
Preschian Febryantara <preschian27@gmail.com>
2016-10-13 23:35:58 +0700

    add root json

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,36 @@ const battle = require('../controller/battle')
 
 module.exports = [{
   method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    const url = `${request.connection.info.protocol}://${request.info.host + request.url.path}`
+    const parse = (link) => url + link
+
+    reply({
+      basePath: url,
+      path: [{
+        title: 'get live matches',
+        url: parse('match/live')
+      }, {
+        title: 'get upcoming matches',
+        url: parse('match/upcoming')
+      }, {
+        title: 'get recent results',
+        url: parse('match/recent')
+      }, {
+        title: 'get match detail',
+        url: parse('battle'),
+        query: {
+          link: {
+            type: 'string',
+            required: true
+          }
+        }
+      }]
+    })
+  }
+}, {
+  method: 'GET',
   path: '/match/{type}',
   handler: (request, reply) => {
     match(encodeURIComponent(request.params.type), data => reply(data))
